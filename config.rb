a4e40467d5586cc77cc0a51395387d86d27f5628
Ross <designpxls@gmail.com>
2018-11-02 14:31:24 -0400

    updated styles and form

--- config.rb
+++ config.rb
@@ -11,9 +11,10 @@ fonts_dir = "fonts"
 http_stylesheets_path = 'css'
 http_javascripts_path = 'js'
 http_images_path = 'img'
+sourcemap = true
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :compressed
+ output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
